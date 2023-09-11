67353a29f31a2229a5516f800e6f6b58d92a9b41
oneraul <oneraul@gmail.com>
2023-09-11 21:06:23 +0200

    Extracted tina tokens into .env (The code generated by the tina cli leaks the tokens by default smh)

--- tina/config.ts
+++ tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "b03c0a9a-b802-4cce-9b66-c702a5f066aa", // Get this from tina.io
-  token: "6bd00339edc80eb41eda0a3d7d93d175bab13d0d", // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
 
   build: {
     outputFolder: "admin",
