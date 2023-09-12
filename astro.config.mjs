368bc36e465882ad41dbcfd1bfc66d39ec2d0574
oneraul <oneraul@gmail.com>
2023-09-12 02:48:24 +0200

    tttt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://oneraul.github.io",
-  //   base: "/astro-tina",
+  base: "/astro-tina",
 });
